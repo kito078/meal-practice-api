8906b171bb4981292bfee81dd091ae410c964c2d
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-18 10:54:33 +0200

    fetch

--- src/components/user/UserResults.js
+++ src/components/user/UserResults.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function UserResults() {
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+  const fetchMeals = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_MEAL_URL}/categories.php`
+    );
+    const data = await response.json();
+
+    console.log(data);
+  };
   return <div>results</div>;
 }
 
